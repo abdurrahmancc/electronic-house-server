4f98c877da91f2456f47e9d67d3af5e546c51486
abdurrahmancc <personalbusinessmail63@gmail.com>
2022-05-07 18:22:11 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const port = process.env.PORT || 5000;
+require("dotenv").config();
+
+// middleware
+app.use(express.json());
+app.use(cors());
+
+// mongodb connect
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASSWORD}@cluster0.y5ior.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const productCollection = client.db("electronicHouse").collection("products");
+
+    app.post("/products", async (req, res) => {
+      const product = req.body;
+      const result = await productCollection.insertOne(product);
+      res.send(result);
+    });
+
+    app.get("/products", async (req, res) => {
+      const query = {};
+      const cursor = productCollection.find(query);
+      const result = await toArray(cursor);
+      res.send(result);
+    });
+  } finally {
+    // await client.close();
+  }
+}
+
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("running my node server");
+});
+
+app.listen(port, () => {
+  console.log("server running port", port);
+});
